72c705ea290f06ae8b8e2bd117ba8a270100fb31
sonchanghyeon <cson90563@gmail.com>
2026-01-10 23:08:52 +0900

    refactor: 목업 PG의 모든 승인을 성공적으로 처리
    partnerId의 홀수 여부를 확인하던 기존 로직을 제거하고, 무조건적인 성공(true)으로 변경합니다.

--- modules/external/pg-client/src/main/kotlin/im/bigs/pg/external/pg/MockPgClient.kt
+++ modules/external/pg-client/src/main/kotlin/im/bigs/pg/external/pg/MockPgClient.kt
@@ -17,7 +17,7 @@ import kotlin.random.Random
  */
 @Component
 class MockPgClient : PgClientOutPort {
-    override fun supports(partnerId: Long): Boolean = partnerId % 2L == 1L
+    override fun supports(partnerId: Long): Boolean = true
 
     override fun approve(request: PgApproveRequest): PgApproveResult {
         val dateOfMonth = LocalDate.now().format(DateTimeFormatter.ofPattern("MMdd"))
